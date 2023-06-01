a96ce53675a38b5ef9a04479f5d549b6f1d368a4
Ashish Singh <ashishsingh21798@gmail.com>
2023-06-01 07:55:23 +0530

    Blog post CRUD Project

---
+++ blog/models/setup.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dsn := "root:root@root@tcp(127.0.0.1:3306)/blog?charset=utf8mb4&parseTime=True&loc=Local"
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	database.AutoMigrate(&Post{})
+
+	DB = database
+}
